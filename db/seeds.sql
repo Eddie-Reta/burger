adf28d038b66f0bc269abc9d116ff6c1eeb89158
Hyper_E <er031991@gmail.com>
2019-06-17 18:12:59 -0400

    created database

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Impossible Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', true);
